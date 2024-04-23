577416cdc1dd1fa7a064ae4d592c520e6110f42d
kennyChukwuebuka <kenkinado@gmail>
2024-04-23 14:41:49 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 """Async
 """
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Async generation function
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
