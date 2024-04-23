52a3f4be6e2c07c4320e382052b6b3111ceddd51
kennyChukwuebuka <kenkinado@gmail>
2024-04-23 17:15:15 +0100

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async
+"""
+
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Async
+    """
+    return [i async for i in async_generator()]
