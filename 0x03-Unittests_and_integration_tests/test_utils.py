7b581c5c086fa20e3a4757a5b85b0fcfcb2a09b5
kennyChukwuebuka <kenkinado@gmail>
2024-05-11 17:21:17 +0100

    paramitarized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Test the access_nested_map function with various
+        nested maps and paths.
+
+        Parameters:
+            nested_map (dict): The nested map to access.
+            path (tuple): The path to the value in the nested map.
+            expected (Any): The expected value at the given path.
+
+        Returns:
+            None
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
