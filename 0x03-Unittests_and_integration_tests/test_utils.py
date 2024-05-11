55eb3b1e6eb8ffcd2a0fca2082ce4a19792bc7c7
kennyChukwuebuka <kenkinado@gmail>
2024-05-11 17:32:52 +0100

    paramitarized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,28 +7,38 @@ import unittest
 from parameterized import parameterized
 
 
+def access_nested_map(data, keys, default=None):
+    """
+    Access nested values in a nested data structure using a sequence of keys.
+
+    Args:
+    - data (dict or list): The nested data structure
+    (e.g., dictionary or list).
+    - keys (iterable): A sequence of keys or indices
+    to traverse the nested structure.
+    - default (optional): The default value to return
+    if the requested value is not found. Default is None.
+
+    Returns:
+    - The value found at the specified keys, or the default value if not found.
+    """
+    try:
+        for key in keys:
+            data = data[key]
+        return data
+    except (KeyError, IndexError, TypeError):
+        return default
+
+
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    Test the access_nested_map function
-    """
+    """Test access_nested_map"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Test the access_nested_map function with various
-        nested maps and paths.
-
-        Parameters:
-            nested_map (dict): The nested map to access.
-            path (tuple): The path to the value in the nested map.
-            expected (Any): The expected value at the given path.
-
-        Returns:
-            None
-        """
+        """Test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
